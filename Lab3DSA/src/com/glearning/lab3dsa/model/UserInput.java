b53a0633c70b60dfd116b545985e41edd8d545e0
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-04-23 18:20:10 +0530

    change method

--- Lab3DSA/src/com/glearning/lab3dsa/model/UserInput.java
+++ Lab3DSA/src/com/glearning/lab3dsa/model/UserInput.java
@@ -2,7 +2,7 @@ package com.glearning.lab3dsa.model;
 
 public class UserInput {
 	String input="";
-	UserInput(String input) {
+	public void setUserInput(String input) {
 		this.input=input;
 	}
 	public String getUserInput() {
