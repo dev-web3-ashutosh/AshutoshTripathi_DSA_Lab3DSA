d4bb4d66bf0adaed05aee995417cccf654b15555
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-04-23 18:30:38 +0530

    write main method

--- Lab3DSA/src/com/glearning/lab3dsa/main/Main.java
+++ Lab3DSA/src/com/glearning/lab3dsa/main/Main.java
@@ -2,14 +2,26 @@ package com.glearning.lab3dsa.main;
 
 import java.util.Scanner;
 
+import com.glearning.lab3dsa.model.UserInput;
+import com.glearning.lab3dsa.service.BalancedBrackets;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Please enter input string of brackets.");
-		String input=sc.nextLine();
-		
-
+		boolean flag=true;
+		UserInput obj=new UserInput();
+		while(flag) {
+			System.out.println("Enter the input  or q to quit::");
+			String input = sc.next();
+			if (input.equals("q")) {
+				flag = false;
+				continue;
+			}
+			obj.setUserInput(input);
+			boolean result = BalancedBrackets.isBalanced(obj.getUserInput());
+			System.out.println("Input is balanced ? "+ result);
+		}
+		sc.close();
 	}
-
 }
