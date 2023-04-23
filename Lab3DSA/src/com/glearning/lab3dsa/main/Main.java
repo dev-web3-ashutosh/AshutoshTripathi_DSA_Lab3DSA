2f857db06595bbe33e6baca74b7ac828fc3bb3a3
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-04-23 17:43:06 +0530

    take user input

--- Lab3DSA/src/com/glearning/lab3dsa/main/Main.java
+++ Lab3DSA/src/com/glearning/lab3dsa/main/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter input string of brackets.");
+		String input=sc.nextLine();
 		
 
 	}
