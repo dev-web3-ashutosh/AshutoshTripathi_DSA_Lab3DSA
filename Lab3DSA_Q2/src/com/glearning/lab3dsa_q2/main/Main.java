1cbad16f20a5a1cf2212f28f94d010d7d3356510
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-04-23 19:18:20 +0530

    insert values in BST

---
+++ Lab3DSA_Q2/src/com/glearning/lab3dsa_q2/main/Main.java
@@ -0,0 +1,22 @@
+package com.glearning.lab3dsa_q2.main;
+
+import com.glearning.lab3dsa_q2.model.BinarySearchTree;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BinarySearchTree tree=new BinarySearchTree();
+		tree.insert(8);
+		tree.insert(3);
+		tree.insert(10);
+		tree.insert(1);
+		tree.insert(6);
+		tree.insert(14);
+		tree.insert(4);
+		tree.insert(7);
+		tree.insert(13);
+		
+
+	}
+
+}
